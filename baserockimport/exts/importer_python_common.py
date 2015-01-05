12ec2e4efe868d990076ed8b4e7376c3e808b38e
Richard Ipsum <richard.ipsum@codethink.co.uk>
2015-01-05 10:23:52 +0000

    Update copyrights

--- baserockimport/exts/importer_python_common.py
+++ baserockimport/exts/importer_python_common.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2014  Codethink Limited
+# -*- coding: utf-8 -*-
+# Copyright © 2014, 2015  Codethink Limited
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
